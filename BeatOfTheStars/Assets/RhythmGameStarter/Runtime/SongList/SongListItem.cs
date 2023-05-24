829b4c0555b4ed6c5b5129ddf5aeba50a0ed3efc
Al Sherlock <al_sherlock95@outlook.com>
2023-05-24 23:52:08 +0100

    Added Rhythm Game Starter asset
    Added in asset to aid with creating the rhythm game

---
+++ BeatOfTheStars/Assets/RhythmGameStarter/Runtime/SongList/SongListItem.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RhythmGameStarter
+{
+    public class SongListItem : MonoBehaviour
+    {
+        public TextMeshProUGUI label;
+        public TextMeshProUGUI authorLabel;
+        public TextMeshProUGUI indexLabel;
+        public Button button;
+        public Image difficultiesFill;
+        public Image backgroundImage;
+    }
+}
