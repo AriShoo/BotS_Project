219047fbb64ac8f3ce512cb3f755334cd5b729b3
Al Sherlock <al_sherlock95@outlook.com>
2023-06-03 04:40:54 +0100

    Tutorial transfer/High scores
    Transferred the rhythm portion of tutorial level over to the scene itself.
    Also began work on the high score system

---
+++ BeatOfTheStars/Assets/Scripts/highScoreTable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highScoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("hsEntryContainer");
+        entryTemplate = entryContainer.Find("hsEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        for (int i = 0; i <10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH"; break;
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
+            entryTransform.Find("placementNo").GetComponent<Text>().text = rankString;
+
+            int score = Random.Range(0, 10000);
+            entryTransform.Find("scoreNo").GetComponent<Text>().text = score.ToString();
+            
+            string name = "AAA";
+            entryTransform.Find("nameEntry").GetComponent<Text>().text = name;
+        }
+    }
+}
