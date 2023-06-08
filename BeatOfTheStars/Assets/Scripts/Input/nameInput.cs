2ab240ec5ee907c4acdd20414d7ef417788b1e5f
Al Sherlock <al_sherlock95@outlook.com>
2023-06-08 03:01:01 +0100

    Fixes

--- BeatOfTheStars/Assets/Scripts/Input/nameInput.cs
+++ BeatOfTheStars/Assets/Scripts/Input/nameInput.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class nameInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    [SerializeField] private TextMeshProUGUI _letter01;
+    [SerializeField] private TextMeshProUGUI _letter02;
+    [SerializeField] private TextMeshProUGUI _letter03;
 
-    // Update is called once per frame
-    void Update()
+    private int currIndex = 0;
+    public void UpArrow()
     {
-        
+        currIndex++;
+        if (currIndex >= alpha.Length)
+            currIndex = 0; // Loop around
+        // Set Text to new Letter
+        _letter01.text = alpha[currIndex].ToString();
+        _letter02.text = alpha[currIndex].ToString();
+        _letter03.text = alpha[currIndex].ToString();
     }
 }
