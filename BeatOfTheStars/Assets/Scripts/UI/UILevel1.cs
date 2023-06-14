3b75112dfc9f25eaa279869d67c8175d3888dec9
AL SHERLOCK <1998942@fife.ac.uk>
2023-06-14 11:31:48 +0100

    Edit Prefab for UI

--- BeatOfTheStars/Assets/Scripts/UI/UILevel1.cs
+++ BeatOfTheStars/Assets/Scripts/UI/UILevel1.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UILevel1 : MonoBehaviour
 {
     [SerializeField] Button _continue;
     [SerializeField] Button _quit;
 
+    ScenesManager ScenesManager;
 
     void Start()
     {
