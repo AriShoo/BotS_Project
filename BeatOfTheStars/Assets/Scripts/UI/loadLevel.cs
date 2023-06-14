8e5c3b537fb05b9af513dd55ac61d9095a7b88dc
AL SHERLOCK <1998942@fife.ac.uk>
2023-06-14 14:44:22 +0100

    Final Demo Build

--- BeatOfTheStars/Assets/Scripts/UI/loadLevel.cs
+++ BeatOfTheStars/Assets/Scripts/UI/loadLevel.cs
@@ -7,14 +7,16 @@ using UnityEngine.SceneManagement;
 public class loadLevel : MonoBehaviour
 {
     [SerializeField] Button toLevel;
+    [SerializeField] Button Quit;
     [HideInInspector]
 
     ScenesManager scenesManager;
-    
+
 
     void Start()
     {
         toLevel.onClick.AddListener(PlayGame);
+        Quit.onClick.AddListener(QuitGame);
     }
     private void PlayGame()
     {
@@ -22,6 +24,15 @@ public class loadLevel : MonoBehaviour
         {
             Debug.Log("Play level 1");
             ScenesManager.instance.LoadScene(ScenesManager.Scene.Level1);
+        }
+    }
+
+    private void QuitGame()
+    {
+        if (Input.GetKey(KeyCode.Keypad7))
+        {
+            Debug.Log("Quit");
+            Application.Quit();
         }
     }
 
